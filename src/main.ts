b6a091f8246e86d924233c93d36f1410a94fba26
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-12-02 00:09:59 +0700

    validate file .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import envConfig from './shared/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envConfig.PORT ?? 3000);
 }
 void bootstrap();
---
+++ src/shared/config.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { plainToInstance } from 'class-transformer';
+import { IsNumber, IsString, validateSync } from 'class-validator';
+import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
+
+// Kiểm tra xem có file .env không
+if (!fs.existsSync(path.resolve('.env'))) {
+  console.log('Không tìm thấy file .env');
+  process.exit(1);
+}
+
+class ConfigSchema {
+  @IsString()
+  DATABASE_URL: string;
+
+  @IsNumber()
+  PORT: number;
+
+  @IsString()
+  ACCESS_TOKEN_SECRET: string;
+
+  @IsString()
+  ACCESS_TOKEN_EXPIRES_IN: string;
+
+  @IsString()
+  REFRESH_TOKEN_SECRET: string;
+
+  @IsString()
+  REFRESH_TOKEN_EXPIRES_IN: string;
+}
+
+const configServer = plainToInstance(ConfigSchema, process.env, {
+  enableImplicitConversion: true,
+});
+const errors = validateSync(configServer);
+
+if (errors.length > 0) {
+  console.log('Các giá trị cấu hình trong file .env không hợp lệ');
+
+  const errorDetails = errors.map((error) => ({
+    property: error.property,
+    constraints: error.constraints,
+    value: error.value,
+  }));
+
+  throw new Error(JSON.stringify(errorDetails, null, 2));
+}
+
+const envConfig = configServer;
+
+export default envConfig;
